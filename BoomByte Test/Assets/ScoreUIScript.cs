24c8092a13375f2929759419f69a60177dfa4b17
MrMeatSlapper <c.antoniadis@outlook.com>
2022-12-07 19:43:49 +0200

    added time UI

--- BoomByte Test/Assets/ScoreUIScript.cs
+++ BoomByte Test/Assets/ScoreUIScript.cs
@@ -10,6 +10,9 @@ public class ScoreUIScript : MonoBehaviour
 
     public Text scoreText;
     public Text attemptsText;
+    public Text timerUI;
+
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,10 @@ public class ScoreUIScript : MonoBehaviour
         scoreText.text = "Score: " + pointsRef.score.ToString();
         //Bug: renamed Attempts: to Moves: as it would not show the integer
         attemptsText.text = "Moves: " +strikeRef.strikeAttempts.ToString();
-        Debug.Log("attemps are: " + strikeRef.strikeAttempts.ToString());
+        timer += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer % 60F);
+        int milliseconds = Mathf.FloorToInt((timer * 100F) % 100F);
+        timerUI.text = minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("00");
     }
 }
