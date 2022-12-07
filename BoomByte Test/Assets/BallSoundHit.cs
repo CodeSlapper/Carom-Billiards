8768802c79d6118857098dc8a5b81af27f14713a
MrMeatSlapper <c.antoniadis@outlook.com>
2022-12-07 21:13:39 +0200

    added sound FX
    added for both menu and ball (depending on velocity)

---
+++ BoomByte Test/Assets/BallSoundHit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSoundHit : MonoBehaviour
+{
+     AudioSource hitEffect;
+    Rigidbody gb;
+    void Start()
+    {
+        hitEffect = GetComponent<AudioSource>();
+        gb = GetComponent<Rigidbody>();
+    }
+    //need to make sure that if two balls strike each other, only one of the sources will play
+    void OnCollisionEnter(Collision collision)
+    {
+        //volume is between 0 and 1, need to divide relativeSound for a more realistic number
+        float relativeSound = (gb.velocity.magnitude) / 20;
+        if (collision.gameObject.tag != "Table")
+        {
+            hitEffect.volume = relativeSound;
+            hitEffect.Play();
+        }
+    }
+}
---
+++ BoomByte Test/Assets/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("generalVolume"))
+        {
+            PlayerPrefs.SetFloat("generalVolume", 1);
+            LoadVolume();
+        }
+        else
+        {
+            LoadVolume();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        SaveVolume();
+    }
+
+    private void LoadVolume()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("generalVolume");
+    }
+    private void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("generalVolume", volumeSlider.value);
+    }
+}
